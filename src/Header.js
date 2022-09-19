1361c27e5b893b1dc0ef35e1362cde5f0e4b7a52
Tumeyy <stefan.tumey@gmail.com>
2022-09-18 21:55:04 -0400

    Essentially complete with header

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import './Header.css'
 import SearchIcon from '@material-ui/icons/Search'
+import HomeIcon from '@material-ui/icons/Home'
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount'
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter'
+import ChatIcon from '@material-ui/icons/Chat'
+import NotificationsIcon from '@material-ui/icons/Notifications'
+import HeaderOption from './HeaderOption'
 
 function Header() {
   return (
@@ -16,7 +22,13 @@ function Header() {
             </div>
         </div>
 
-        <div className='.header__right'>
+        <div className='header__right'>
+            <HeaderOption Icon={HomeIcon} title='Home' />
+            <HeaderOption Icon={SupervisorAccountIcon} title='My Network' />
+            <HeaderOption Icon={BusinessCenterIcon} title='Jobs' />
+            <HeaderOption Icon={ChatIcon} title='Messaging' />
+            <HeaderOption Icon={NotificationsIcon} title='Notifications' />
+            <HeaderOption avatar='https://lh3.googleusercontent.com/a-/ACNPEu_GIdaFRyNvii7Y8guD9AXSvK056T05FcqBWPwJow=s288-p-rw-no' title='me' />
 
         </div>
     </div>
